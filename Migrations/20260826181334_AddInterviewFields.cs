12f91626f98e4960ad2eee5eeecafd31d44700eb
Christopher Reeves <christopherreeves@Christophers-MacBook-Air.local>
2026-08-26 13:29:28 -0500

    Add migration files for interview fields

---
+++ Migrations/20260826181334_AddInterviewFields.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsiderCareers.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddInterviewFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InterviewRoom",
+                table: "JobApplications",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InterviewStatus",
+                table: "JobApplications",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InterviewRoom",
+                table: "JobApplications");
+
+            migrationBuilder.DropColumn(
+                name: "InterviewStatus",
+                table: "JobApplications");
+        }
+    }
+}
